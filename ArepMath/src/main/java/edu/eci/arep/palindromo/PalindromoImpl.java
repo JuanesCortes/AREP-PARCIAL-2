d2be8e8523b16d649c12a9fdc8fa59fe112a7c37
unknown <juan.cortes-p@labinfo.is.escuelaing.edu.co>
2022-10-26 18:40:20 -0500

    respuesta json

--- ArepMath/src/main/java/edu/eci/arep/palindromo/PalindromoImpl.java
+++ ArepMath/src/main/java/edu/eci/arep/palindromo/PalindromoImpl.java
@@ -5,16 +5,17 @@ package edu.eci.arep.palindromo;
  * @author juan.cortes-p
  */
 public class PalindromoImpl {
-    public static String isPalindromo(String cadena){
+
+    public static String isPalindromo(String cadena) {
         boolean valid = true;
-        for (int i = 0; i < cadena.length(); i++){
-            if (!cadena.substring(i, i+1).equals(cadena.substring(cadena.length()-1 - i, cadena.length() - i))){
+        for (int i = 0; i < cadena.length(); i++) {
+            if (!cadena.substring(i, i + 1).equals(cadena.substring(cadena.length() - 1 - i, cadena.length() - i))) {
                 valid = false;
             }
         }
-        String res = "Si es palíndromo";
-        if (!valid){
-            res = "No es palíndromo";
+        String res = "{\"operation\": \"palíndromo\", \"input\":  \""+cadena+"\", \"output\":  \"Si es palíndromo\"}";
+        if (!valid) {
+            res = "{\"operation\": \"palíndromo\", \"input\":  \""+cadena+"\", \"output\":  \"No es palíndromo\"}";
         }
         return res;
     }
